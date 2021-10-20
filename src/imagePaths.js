a3451b7f363d739e3db39206100f280a1689a555
Kombiz Khayami <kombizkhayami@outlook.com>
2021-10-20 18:15:12 -0400

    converted site to react

---
+++ src/imagePaths.js
@@ -0,0 +1,135 @@
+ const images = 
+{
+  icons : {    
+      nothing: {
+        name: '',
+        path: require('./images/small.nothing.icon.png').default
+      },
+      bleed: {
+        name: '',
+        path: require('./images/bleed.png').default
+      },
+      blight: {
+        name: '',
+        path: require('./images/blight.png').default
+      },
+      quirkNeg: {
+        name: '',
+        path: require('./images/small.Quirk-neg.icon.png').default
+      },
+      quirkPos: {
+        name: '',
+        path: require('./images/small.Quirk-pos.icon.png').default
+      },
+      stress: {
+        name: '',
+        path: require('./images/small-stress.icon.png').default
+      },
+      stressHeal: {
+        name: '',
+        path: require('./images/Heal_stress.icon.png').default
+      },
+      portrait: {
+        name: '',
+        path: require('./images/small-portrait.icon.png').default
+      },
+      scout: {
+        name: '',
+        path: require('./images/Scout_Ahead.png').default
+      },
+      loot: {
+        name: '',
+        path: require('./images/loot.icon.png').default
+      },
+      disease: {
+        name: '',
+        path: require('./images/disease.png').default
+      },
+      firewood: {
+        name: '',
+        path: require('./images/firewood.png').default
+      },
+      summon: {
+        name: '',
+        path: require('./images/summon.png').default
+      },
+      theBlood: {
+        name: '',
+        path: require('./images/The_Blood.png').default
+      } 
+  },
+
+  curios : {
+    bloodflowers: {
+      name: '',
+      path: require('./images/Bloodflowers.png').default
+    },
+    damedFountain: {
+      name: '',
+      path: require('./images/Damned_Fountain.png').default
+    },
+    disturbingDiversion: {
+      name: '',
+      path: require('./images/Disturbing_Diversion.png').default
+    },
+    forgottenDelicacies: {
+      name: '',
+      path: require('./images/Forgotten_Delicacies.png').default
+    },
+    hoodedShrew: {
+      name: '',
+      path: require('./images/Hooded_Shrew.png').default
+    },
+    wizenedShrew: {
+      name: '',
+      path: require('./images/Wizened_Shrew.png').default
+    },
+    pileOfStrangeBones: {
+      name: '',
+      path: require('./images/Pile_of_Strange_Bones.png').default
+    },
+    throbbingCoccoons: {
+      name: '',
+      path: require('./images/Throbbing_Coccoons.png').default
+    },
+    throbbingHive: {
+      name: '',
+      path: require('./images/Throbbing_Hive.png').default
+    },
+    wineCrate: {
+      name: '',
+      path: require('./images/Wine_Crate.png').default
+    }
+
+  },
+
+  activators : {
+    torch: {
+      name: '',
+      path: require('./images/Torch.png').default
+    },
+    bandages: {
+      name: '',
+      path: require('./images/Bandage.png').default
+    },
+    shovel: {
+      name: '',
+      path: require('./images/Shovel.png').default
+    },
+    medicinalHerbs: {
+      name: '',
+      path: require('./images/Medicinal_Herbs.png').default
+    },
+    theBlood: {
+      name: '',
+      path: require('./images/The_Blood.png').default
+    },
+    holyWater: {
+      name: '',
+      path: require('./images/Holy_Water.png').default
+    },
+
+  }
+}
+
+export { images as default };
